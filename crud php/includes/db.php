35333bbe1de5be55006dbd04b51c567772b5b993
caio <caio.f04@aluno.ifsc.edu.br>
2023-11-27 09:54:43 -0300

    db

--- crud php/includes/db.php
+++ crud php/includes/db.php
@@ -7,9 +7,9 @@ $database = 'docinhos';
 // Conexão com o banco de dados
 $conn = new mysqli($hostname, $username, $password, $database);
 
-
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
+echo "Conexão bem-sucedida!";
 ?>
